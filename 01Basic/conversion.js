e61d13ebf81ec5e10b2468d48286a73658221c7c
SumitTeerthani <sumitteerthani2002@gmail.com>
2023-11-04 13:51:52 +0530

    operations

--- 01Basic/conversion.js
+++ 01Basic/conversion.js
@@ -1,4 +1,12 @@
-let score="33";
-// let value=Number(score);
-console.log(typeof score); 
-console.log(score);
+// let score="33";
+// // let value=Number(score);
+// console.log(typeof score); 
+// console.log(score);
+
+// Operations
+let value=3;
+let negvalue=-value
+console.log(negvalue) //-3
+console.log(2**3);  //8
+console.log(true)   //true
+console.log(+true) //1
