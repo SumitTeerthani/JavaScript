6f56208660ec2fe5370df42bcaff66dab90564a1
SumitTeerthani <sumitteerthani2002@gmail.com>
2023-11-06 20:45:59 +0530

    Learning About Objects

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,21 @@
+
+const mys=Symbol("keys");
+const JsUser={
+    name:"Sumit",
+    age : 21,
+    College:"NIT Jalandhar",
+    [mys]:"Mykeys"
+}
+
+// console.log(JsUser);
+// console.log(typeof(JsUser[mys]));
+// JsUser.age=20;
+// console.log(JsUser)
+// Object.freeze(JsUser);  //It freze the object so no change Can Occur Now
+// JsUser.age=23;
+// console.log(JsUser);
+
+JsUser.greetings=function(){
+    console.log(`Hello Js User , ${this.name}`)
+}
+JsUser.greetings();
