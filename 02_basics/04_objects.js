26b3340bc6ee3c19966fb77e4182076d77887f50
SumitTeerthani <sumitteerthani2002@gmail.com>
2023-11-09 19:06:11 +0530

    JSON Into and Object Destructure

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -3,4 +3,19 @@ const tinderUser=new Object();
 tinderUser.name="Manan"
 tinderUser.age=21;
 
-console.log(tinderUser) //Gives simple object Output
+//console.log(tinderUser) //Gives simple object Output
+
+
+const course={
+    name:"Web Development",
+    duration:3,
+    courseInstructor:"Hitesh Chaudhary"
+}
+
+const {courseInstructor:instructor}=course
+console.log(`The instructor of the ${course.name} is ${instructor}`);
+
+
+
+//JSON -:
+// { }  this is JSON Format 
