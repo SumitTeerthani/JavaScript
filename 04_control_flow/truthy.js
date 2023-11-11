0b9193dc774e365d948cb8346c2767c82674a432
SumitTeerthani <sumitteerthani2002@gmail.com>
2023-11-11 17:24:09 +0530

    Control Flow in js

---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,16 @@
+// //False values
+
+// false,0,-0,BigInt 0n ,"",undefined,null,NaN
+
+// //inke alava sbko true mana jata hai
+
+
+// //truthy value
+// "0",'false'," ",[],{} function(){},
+const empty={}
+
+if(Object.keys(empty).length==0){
+    console.log("Empty Object");
+}
+
+//Nullish Coalescing Operator   -> null or undefined
