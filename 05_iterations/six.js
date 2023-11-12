5e60073b624631af9d952379f1f4cf6a3b02cf3a
SumitTeerthani <sumitteerthani2002@gmail.com>
2023-11-13 01:00:31 +0530

    filter in js

---
+++ 05_iterations/six.js
@@ -0,0 +1,20 @@
+const names=["Agrim","Anupam","Gaurav","Vinay"];
+
+// const printNames= names.forEach((item)=>{
+//     //console.log(item );
+//     return item;
+// })
+// console.log(printNames); //undefined
+
+
+const myNums=[1,2,3,4,5,6,7,8,9,10];
+
+const values= myNums.filter((item)=> item>2)
+
+console.log(values);//This will print greater than 2
+
+const values2= myNums.filter((item)=> {item>2}) // this will return empty array we have to use return keyword to make it properly work
+console.log(values2);
+
+const values3= myNums.filter((item)=> { return item>=2})
+console.log(values3);
